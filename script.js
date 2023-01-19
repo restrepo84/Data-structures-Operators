cbf2eb6c9a2d149e84b7d5dcfb6e2de8ce7a06da
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-01-19 12:40:08 -0700

    default values

--- script.js
+++ script.js
@@ -60,9 +60,12 @@ console.log(restaurant.order(2, 0));
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
+// nested destructuring
 const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
-// console.log(i, j);
-
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
+
+// default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
