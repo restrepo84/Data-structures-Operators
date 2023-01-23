3cca2d4559c9d0bb4d0e112d0c6d8aa52e51ccb7
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-01-23 11:16:47 -0700

    Logical Assignment Operators

--- script.js
+++ script.js
@@ -54,14 +54,45 @@ const restaurant = {
   },
 };
 
+//* Logical Assignment Operators *//
+
+const rest1 = {
+  name: "Capri",
+  //numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni Rossi",
+};
+// Or assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator means (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+//rest1.owner = rest1.owner && "<ANONYMOUS>";
+//rest2.owner = rest2.owner && "<ANONYMOUS>";
+rest1.numGuests &&= "<ANONYMOUS>";
+rest2.numGuests &&= "<ANONYMOUS>";
+
+console.log(rest1);
+console.log(rest2);
+
 //* The Nullish Operator (??) *//
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
 
 // Nullish: null and undefined  (NOT 0 or "" )
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
 
 //* Short Circuiting (&& and ||) *//
 
